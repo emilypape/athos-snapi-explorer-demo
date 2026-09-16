02d98e040c4ae2bdedbdac75ebe12aeaecc2a4cf
Emily Pape <emilypape@Emilys-MacBook-Pro.local>
2026-09-16 16:57:15 -0600

    Header: keep only the snap-client info link, drop docs/GitHub icons
    Per Emily - top right now shows just the "i" link to the snap-client
    repo. Removed the OpenAPI docs link and the personal-repo GitHub icon,
    along with their now-unused image assets.

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,7 +1,5 @@
 import { h, Component } from 'preact';
 import athosLogo from '../../../public/images/athos_logo.svg';
-import openApiLogo from '../../../public/images/openapi_logo.png';
-import githubLogo from '../../../public/images/github_logo.svg';
 
 export class Header extends Component {
 	render() {
@@ -15,12 +13,6 @@ export class Header extends Component {
 				<div class="links">
 					<a href="https://github.com/AthosCommerce/snap/tree/main/packages/snap-client" target="_blank">
 						<span class="icon large readme">i</span>
-					</a>
-					<a href="https://docs.athoscommerce.com/reference" target="_blank">
-						<img class="icon img" src={openApiLogo} />
-					</a>
-					<a href="https://github.com/emilypape/athos-snapi-explorer-demo" target="_blank">
-						<img class="icon img" src={githubLogo} />
 					</a>
 				</div>
 			</div>
